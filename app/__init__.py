22407d63bde26f4ad1bd7ff4c541495550804dd8
Lucas Fiais <lucas.fiais@hotmail.com>
2026-08-05 09:50:57 -0300

    Configuração PostgreSQL para produção

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.secret_key = "barbearia_fiais_2026"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///barbearia.db"
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///barbearia.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from app import app, db
-from app.models import Usuario
-with app.app_context():
-    db.create_all()
+from app import app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
